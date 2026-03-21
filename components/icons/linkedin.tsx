d15e3da87964a3f7f0eb3d2b3772bedc91a3fd2f
Xharlessnow <ekekwecharles50@gmail.com>
2026-03-21 08:57:41 +0100

    Add interests feature

--- components/icons/linkedin.tsx
+++ components/icons/linkedin.tsx
@@ -23,10 +23,10 @@ export default function LinkedIn({
       className={`lucide lucide-linkedin-icon lucide-linkedin ${className}`}
       {...props}
     >
+      <title>LinkedIn</title>
       <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
       <rect width="4" height="12" x="2" y="9" />
       <circle cx="4" cy="4" r="2" />
-      <title>LinkedIn</title>
     </svg>
   );
 }
