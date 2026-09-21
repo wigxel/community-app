99e18878c6ec85ccf022bed66fa65c031b918259
Joseph Owonvwon <joseph.owonwo@gmail.com>
2026-09-21 11:57:36 +0000

    Improvement to the Talents page (#136)
    * wip: improving talents page presentation
    
    * feat: hide filter logic for later redesign
    
    * feat: a user can see more information on a user profile
    
    * refactor: resolved biome warnings
    
    * feat(public): set the url as the SSoT for search input in landing and talents page
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- components/profile/avatar.tsx
+++ components/profile/avatar.tsx
@@ -1,22 +1,39 @@
+import { BadgeIcon } from "lucide-react";
 import { safeStr } from "~/lib/data.helpers";
 import { cn } from "~/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+
 export type ProfileAvatarProps = {
   name: string;
   src?: string;
   className?: string;
+  verified?: boolean;
 };
+
 export function ProfileAvatar(props: ProfileAvatarProps) {
-  const { name, src, className } = props;
+  const { name, src, className, verified = false } = props;
 
   const initial = safeStr(name).charAt(0).toUpperCase();
 
   return (
-    <Avatar className={cn("size-14", className)}>
-      <AvatarImage src={src || undefined} />
-      <AvatarFallback className={cn("bg-blue-500/20 text-blue-300", className)}>
-        {initial}
-      </AvatarFallback>
-    </Avatar>
+    <div className="group relative inline-flex">
+      <Avatar className={cn("size-14", className)}>
+        <AvatarImage src={src || undefined} />
+        <AvatarFallback
+          className={cn("bg-blue-500/20 text-blue-300", className)}
+        >
+          {initial}
+        </AvatarFallback>
+      </Avatar>
+
+      {!verified ? null : (
+        <span className="absolute -inset-4 rounded-full p-0.5">
+          <BadgeIcon
+            strokeWidth="0.25"
+            className="text-brand-primary animate group-hover:animation-duration-[10s] h-full w-full group-hover:animate-spin"
+          />
+        </span>
+      )}
+    </div>
   );
 }
--- convex/seeds.ts
+++ convex/seeds.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/** biome-ignore-all lint/suspicious/noExplicitAny: Not relevant */
 import { v } from "convex/values";
 import { internal } from "./_generated/api";
 import { internalMutation } from "./_generated/server";
